69c1fa71045db0d385c4ebef135059d6c597ad1e
David Sarmiento <dsar@uma.es>
2025-08-28 09:05:38 +0200

    Project Initialized

---
+++ nextTasks/src/main/java/com/nextTasks/NextTasksApplication.java
@@ -0,0 +1,13 @@
+package com.nextTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextTasksApplication.class, args);
+	}
+
+}
